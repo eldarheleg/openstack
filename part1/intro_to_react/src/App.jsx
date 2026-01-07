adbbf7d45e2e266e000b2119b14da07ee67976ac
Eldar H. <92878793+eldarheleg@users.noreply.github.com>
2026-01-07 11:06:49 +0300

    Excercise 1.1

--- part1/intro_to_react/src/App.jsx
+++ part1/intro_to_react/src/App.jsx
@@ -1,11 +1,27 @@
+import Content from "./components/Content"
+import Header from "./components/Header"
+import Total from "./components/Total"
+
 const App = () => {
-  const now = new Date()
-  const a = 112
-  const b = 12
-  console.log(a+b)
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
   return (
     <div>
-      <p>Hello world, it is {now.toDateString()}</p>
+      <Header course={course} />
+      <Content 
+        part1={part1} exercises1={exercises1}
+        part2={part2} exercises2={exercises2}
+        part3={part3} exercises3={exercises3}
+      />
+      <Total
+        total={exercises1 + exercises2 + exercises3}
+      />
     </div>
   )
 }
---
+++ part1/intro_to_react/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = (props) => {
+  return (
+    <header className="header">
+      <h1>{props.course}</h1>
+    </header>
+  );
+};
+
+export default Header;
