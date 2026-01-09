f01286f6b944cf483aaa10f3a083f8ab9d87e7b2
Eldar H. <92878793+eldarheleg@users.noreply.github.com>
2026-01-09 16:50:26 +0300

    Exercise 1.4

--- part1/intro_to_react/src/App.jsx
+++ part1/intro_to_react/src/App.jsx
@@ -1,21 +1,24 @@
-import Content from "./components/Content"
-import Header from "./components/Header"
-import Total from "./components/Total"
+import Content from "./components/Content";
+import Header from "./components/Header";
+import Total from "./components/Total";
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const course = "Half Stack application development";
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
+
   // const course = 'Half Stack application development'
   // const part1 = 'Fundamentals of React'
   // const exercises1 = 10
@@ -27,16 +30,10 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content 
-        part1={part1.name} exercises1={part1.exercises}
-        part2={part2.name} exercises2={part2.exercises}
-        part3={part3.name} exercises3={part3.exercises}
-      />
-      <Total
-        total={part1.exercises + part2.exercises + part3.exercises}
-      />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
