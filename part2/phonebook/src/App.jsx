13cf3ac6b6081ecdfe3d8c881c4203dc931ce7d2
Eldar H. <92878793+eldarheleg@users.noreply.github.com>
2026-01-13 05:04:21 +0300

    Exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", phoneNumber: "040-123456" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", phoneNumber: "040-123456", id: 1 },
+    { name: "Ada Lovelace", phoneNumber: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", phoneNumber: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", phoneNumber: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
 
   const onSubmitState = (event) => {
     event.preventDefault();
@@ -16,6 +22,7 @@ const App = () => {
     setNewName("");
     setNewNumber("");
   };
+
   const duplicateCheck = (name) => {
     for (let person of persons) {
       if (person.name === name) {
@@ -25,9 +32,32 @@ const App = () => {
     }
   };
 
+  const listToShow = () => {
+    return filter
+      ? [...persons]
+          .filter((person) =>
+            person.name.toLowerCase().includes(filter.toLowerCase())
+          )
+          .map((person, index) => (
+            <p key={index}>
+              {person.name} - {person.phoneNumber}
+            </p>
+          ))
+      : [...persons].map((person, index) => (
+          <p key={index}>
+            {person.name} - {person.phoneNumber}
+          </p>
+        ));
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with:{" "}
+        <input value={filter} onChange={(e) => setFilter(e.target.value)} />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={onSubmitState}>
         <div>
           name:{" "}
@@ -35,16 +65,18 @@ const App = () => {
         </div>
         <div>
           phone:{" "}
-          <input type="tel" value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
+          <input
+            type="tel"
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {[...persons].map((person, index) => (
-        <p key={index}>{person.name} - {person.phoneNumber}</p>
-      ))}
+      {listToShow()}
     </div>
   );
 };
