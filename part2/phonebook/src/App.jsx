5e966eb86e5afaf91f5c6736af0c3effda2aa67c
Eldar H. <92878793+eldarheleg@users.noreply.github.com>
2026-01-13 04:41:46 +0300

    Exercise 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,26 +6,38 @@ const App = () => {
 
   const onSubmitState = (event) => {
     event.preventDefault();
+    duplicateCheck(newName);
     const personObject = {
       name: newName,
     };
     setPersons(persons.concat(personObject));
     setNewName("");
-  }
+  };
+  const duplicateCheck = (name) => {
+    for (let person of persons) {
+      if (person.name === name) {
+        alert(`${name} is already added to phonebook`);
+        throw new Error("Duplicate name");
+      }
+    }
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={onSubmitState}>
         <div>
-          name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+          name:{" "}
+          <input value={newName} onChange={(e) => setNewName(e.target.value)} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {[...persons].map((person, index) => <p key={index}>{person.name}</p>)}
+      {[...persons].map((person, index) => (
+        <p key={index}>{person.name}</p>
+      ))}
     </div>
   );
 };
