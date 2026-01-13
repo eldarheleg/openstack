24db95ca502907090187897cc6ac81cd7205bf42
Eldar H. <92878793+eldarheleg@users.noreply.github.com>
2026-01-13 03:59:07 +0300

    Exercise 2.3

--- part2/course_info/src/components/Content.jsx
+++ part2/course_info/src/components/Content.jsx
@@ -1,14 +1,17 @@
-import Part from "./Part"
+import Part from "./Part";
 
-const Content = ( {parts} ) => {
-    const sum = parts.reduce((acc, part) => acc + part.exercises, 0);
-    return (
-        <div>
-            {parts.map(part =>
-                <Part part={part} />
-            )}
-            <p>Total of {sum} exercises</p>
-        </div>
-    )
-}   
-export default Content
+const Content = ({ parts }) => {
+  const sum = parts.reduce((acc, part) => {
+    console.log(acc, part.exercises);
+    return acc + part.exercises;
+  }, 0);
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part part={part} />
+      ))}
+      <p>Total of {sum} exercises</p>
+    </div>
+  );
+};
+export default Content;
