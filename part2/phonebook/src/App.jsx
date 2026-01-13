7fe1c70907efd8eb699d438b8179cc4aa057e17d
Eldar H. <92878793+eldarheleg@users.noreply.github.com>
2026-01-13 04:49:52 +0300

    Exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", phoneNumber: "040-123456" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const onSubmitState = (event) => {
     event.preventDefault();
     duplicateCheck(newName);
     const personObject = {
       name: newName,
+      phoneNumber: newNumber,
     };
     setPersons(persons.concat(personObject));
     setNewName("");
+    setNewNumber("");
   };
   const duplicateCheck = (name) => {
     for (let person of persons) {
@@ -31,12 +34,16 @@ const App = () => {
           <input value={newName} onChange={(e) => setNewName(e.target.value)} />
         </div>
         <div>
+          phone:{" "}
+          <input type="tel" value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {[...persons].map((person, index) => (
-        <p key={index}>{person.name}</p>
+        <p key={index}>{person.name} - {person.phoneNumber}</p>
       ))}
     </div>
   );
