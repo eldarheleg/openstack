671b6235435d129da6bd3161a92900417b271fc7
Eldar H. <92878793+eldarheleg@users.noreply.github.com>
2026-01-15 06:00:32 +0300

    Exercise 2.19

--- part2/countries/src/components/Country.jsx
+++ part2/countries/src/components/Country.jsx
@@ -1,19 +1,29 @@
-const Countries = ({country}) => {
-    return (
-        <div>
-            <h1>{country.name.common}</h1>
-            <p>Capital: {country.capital}</p>
-            <p>Area: {country.area}</p>
-            <br />
-            <h2>Languages:</h2>
-            <ul>
-                {Object.values(country.languages).map((language) => (
-                    <li key={language}>{language}</li>
-                ))}
-            </ul>
-            <img src={country.flags.png} alt={`Flag of ${country.name.common}`} />
-        </div>
-    )
-}
+const Country = ({ country }) => {
+  const capital = country.capital?.[0] || "N/A";
+  const languages = country.languages || {};
 
-export default Countries;
+  return (
+    <div>
+      <h2>{country.name.common}</h2>
+      <div>
+        <p>capital {capital}</p>
+        <p>area {country.area} km²</p>
+      </div>
+      <div>
+        <h3>languages</h3>
+        <ul>
+          {Object.values(languages).map((lang) => (
+            <li key={lang}>{lang}</li>
+          ))}
+        </ul>
+      </div>
+      <img
+        src={country.flags.png}
+        alt={`${country.name.common} flag`}
+        width="100"
+      />
+    </div>
+  );
+};
+
+export default Country;
--- part2/countries/src/components/CountryResults.jsx
+++ part2/countries/src/components/CountryResults.jsx
@@ -1,13 +1,37 @@
+import { useState } from "react";
 import Country from "./Country";
+import "./components.css";
 
 const CountryResults = ({ filteredCountries, query }) => {
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
   if (!query) return null;
-  if (filteredCountries.length > 10) return <p>Too many matches...</p>;
+  if (filteredCountries.length > 10)
+    return <p>Too many matches, specify another filter</p>;
   if (filteredCountries.length === 1)
     return <Country country={filteredCountries[0]} />;
-  return filteredCountries.map((country) => (
-    <p key={country.cca2}>{country.name.common}</p>
-  ));
+
+  const handleToggle = (countryCode) => {
+    setSelectedCountry(selectedCountry === countryCode ? null : countryCode);
+  };
+
+  return (
+    <>
+      {filteredCountries.map((country) => (
+        <div key={country.cca2} className="country">
+          <div className="country-header">
+            <span>{country.name.common}</span>
+            <button onClick={() => handleToggle(country.cca2)}>
+              {selectedCountry === country.cca2 ? "hide" : "show"}
+            </button>
+          </div>
+          <div className="country-details">
+            {selectedCountry === country.cca2 && <Country country={country} />}
+          </div>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default CountryResults;
