bd6363b7b08bc0277006c0cd7835b02a0577589a
Eldar H. <92878793+eldarheleg@users.noreply.github.com>
2026-01-27 02:45:35 +0300

    Exercise 3.15

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -4,9 +4,10 @@ const Person = require("./models/person.js");
 
 const app = express();
 
+app.use(express.static("dist"));
 app.use(express.json());
 
-app.use(express.static("dist"));
+
 
 
 app.get("/", (request, response) => {
@@ -83,6 +84,22 @@ app.delete("/api/persons/:id", (request, response) => {
     response.status(404).end();
   }
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
